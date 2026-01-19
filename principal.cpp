d8398711c0cad2405310d92332c252f375df2b23
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-19 20:20:51 -0300

    Feature: hmm

--- principal.cpp
+++ principal.cpp
@@ -3,10 +3,36 @@ using namespace std;
 
 int main()
 {
-    cout << "O que deseja fazer?\n"
-         << "1 - Visualizar\n"
-         << "2 - Adicionar\n"
-         << "3 - Excluir\n";
+    int num;
+    cin >> num;
 
+    cout << "O que deseja fazer?\n"
+         << "0 - Encerrar o programa\n"
+         << "1 - Visualizar o numero\n"
+         << "2 - Multiplicar o numero por dois\n"
+         << "3 - Dividir o numero por dois\n";
+
+    int escolha;
+    cin >> escolha;
+
+    switch (escolha)
+    {
+    case 1:
+        cout << num << "\n";
+        break;
+
+    case 2:
+        num = num * 2;
+        cout << num << "\n";
+        break;
+
+    case 3:
+        num = num / 2;
+        cout << num << "\n";
+        break;
+
+    default:
+        break;
+    }
     return 0;
 }
