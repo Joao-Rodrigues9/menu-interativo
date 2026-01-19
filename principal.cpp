1d808e871000a4709641ab28194cb236139db4e9
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-19 19:58:52 -0300

    Feature - criação de principal

---
+++ principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "O que deseja fazer?\n"
+         << "1 - Visualizar\n"
+         << "2 - Adicionar\n"
+         << "3 - Excluir\n";
+
+    return 0;
+}
