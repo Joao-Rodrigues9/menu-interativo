8e132d2ec4909e17432fb0b64dadab59de5030a8
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-21 19:04:04 -0300

    conversões WIP, terminar elas

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 void calculadora();
 double iniciarCalc();
 void printarMenuCalc(double valorAtual);
+void converterTemperatura();
+
 int main()
 {
     std::cout << "0- Encerrar programa\n"
@@ -109,4 +111,41 @@ void printarMenuCalc(double valorAtual)
               << "Valor atual: " << valorAtual << "\n"
               << "-------------------------------\n"
               << "O que deseja fazer?\n";
+}
+
+void converterTemperatura()
+{
+    char tipoAtual;
+    char tipoDesejado;
+
+    while (true)
+    {
+        std::cout << "Deseja converter a temperatura como?\n";
+        std::cout << "(Utilize C - Celsius; F - Fahrenheit; K - Kelvin)\n";
+
+        std::cout << "De: ";
+
+        std::cin >> tipoAtual;
+
+        std::cout << "Para: ";
+
+        std::cin >> tipoDesejado;
+
+        double temperatura;
+        std::cout << "Informe a temperatura: ";
+        std::cin >> temperatura;
+
+        if (tipoAtual == 'C' && tipoDesejado == 'F')
+        {
+            temperatura = temperatura * 1.8 + 32;
+            tipoAtual = tipoDesejado;
+            std::cout << temperatura << tipoAtual << "\n";
+        }
+        if (tipoAtual == 'C' && tipoDesejado == 'K')
+        {
+            temperatura = temperatura + 273.15;
+            tipoAtual = tipoDesejado;
+            std::cout << temperatura << tipoAtual << "\n";
+        }
+    }
 }
