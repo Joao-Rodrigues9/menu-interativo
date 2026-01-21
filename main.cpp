eb9fe632c8e9308e88e7993142dda322ee9fecd0
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-21 14:55:03 -0300

    Feature: conversões adicionadas

--- main.cpp
+++ main.cpp
@@ -4,20 +4,35 @@ int main()
     char tipoAtual;
     char tipoDesejado;
 
-    std::cout << "Deseja converter a temperatura como?\n";
-    std::cout << "(Utilize C - Celsius; F - Fahrenheit; K - Kelvin)\n";
+    while (true)
+    {
+        std::cout << "Deseja converter a temperatura como?\n";
+        std::cout << "(Utilize C - Celsius; F - Fahrenheit; K - Kelvin)\n";
 
-    std::cout << "De: ";
+        std::cout << "De: ";
 
-    std::cin >> tipoAtual;
+        std::cin >> tipoAtual;
 
-    std::cout << "Para: ";
+        std::cout << "Para: ";
 
-    std::cin >> tipoDesejado;
+        std::cin >> tipoDesejado;
 
-    double temperatura;
-    std::cout << "Informe a temperatura: ";
-    std::cin >> temperatura;
+        double temperatura;
+        std::cout << "Informe a temperatura: ";
+        std::cin >> temperatura;
 
+        if (tipoAtual == 'C' && tipoDesejado == 'F')
+        {
+            temperatura = temperatura * 1.8 + 32;
+            tipoAtual = tipoDesejado;
+            std::cout << temperatura << tipoAtual << "\n";
+        }
+        if (tipoAtual == 'C' && tipoDesejado == 'K')
+        {
+            temperatura = temperatura + 273.15;
+            tipoAtual = tipoDesejado;
+            std::cout << temperatura << tipoAtual << "\n";
+        }
+    }
     return 0;
 }
