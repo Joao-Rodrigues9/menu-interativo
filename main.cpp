f3f68bd0aaf4a12e9990a1f2fe2c352dfd7fee10
João Rodrigues <joaovitor550dito@gmail.com>
2026-01-21 14:40:04 -0300

    Feature: adicionei temperatura e sua inserção

--- main.cpp
+++ main.cpp
@@ -4,14 +4,20 @@ int main()
     char tipoAtual;
     char tipoDesejado;
 
-    std::cout << "Deseja converter a temperatura como?";
-    std::cout << "De:";
+    std::cout << "Deseja converter a temperatura como?\n";
+    std::cout << "(Utilize C - Celsius; F - Fahrenheit; K - Kelvin)\n";
+
+    std::cout << "De: ";
 
     std::cin >> tipoAtual;
 
-    std::cout << "Para:";
+    std::cout << "Para: ";
 
     std::cin >> tipoDesejado;
+
+    double temperatura;
+    std::cout << "Informe a temperatura: ";
+    std::cin >> temperatura;
 
     return 0;
 }
